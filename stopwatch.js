11ebf05b0213f7bcd9124be73c6695d02498536e
Reallyaravind <101699686+Reallyaravind@users.noreply.github.com>
2023-11-04 20:18:57 +0530

    Update stopwatch.js

--- stopwatch.js
+++ stopwatch.js
@@ -5,3 +5,10 @@ function pingCounter(){
 	counter=counter+1;
 }
 setInterval(pingCounter,1000);
+var ctr2=0;
+for(var i=0;i<10000000000;i++){
+	ctr2=ctr2+2;
+}
+console.log(ctr2);
+
+//the above is a example of asynchonous code
